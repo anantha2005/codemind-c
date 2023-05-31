4b0f06743de3b03d7ae738a14d24f5ca22fd633e
Anantha Lakshmi <119730365+anantha2005@users.noreply.github.com>
2023-05-31 21:22:50 +0530

    #include<stdio.h> int main() {     int n,s=0,p=1,d;     scanf("%d",&n);     for(;n>0;)     {         d=n%10;         s=s+d;         p=p*d;         n=n/10;     }     if(s==p)     {         printf("Spy Number");     }     else     {      printf("Not Spy Number");     } }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,q,r,s=0,p=1;
+    int n,s=0,p=1,d;
     scanf("%d",&n);
-    q=n;
-    while(q!=0)
+    for(;n>0;)
     {
-        r=q%10;
-        s=s+r;
-        p=p*r;
-        q=q/10;
+        d=n%10;
+        s=s+d;
+        p=p*d;
+        n=n/10;
     }
     if(s==p)
     {
@@ -17,6 +16,6 @@ int main()
     }
     else
     {
-        printf("Not Spy Number");
+     printf("Not Spy Number");  
     }
 }
